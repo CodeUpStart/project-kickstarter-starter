e4ad109a08bd4037a113365e0ee69082e33d377c
Brandon Chez <chezmix@gmail.com>
2015-10-29 10:23:20 -0400

    Add heroku deployment files

--- punchstarter/default_settings.py
+++ punchstarter/default_settings.py
@@ -1,8 +1,8 @@
 import os 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DEBUG=True
-SQLALCHEMY_DATABASE_URI="sqlite:///"+ BASE_DIR + "/app.db"
-SQLALCHEMY_TRACK_MODIFICATIONS=True
+DEBUG = os.environ.get('DEBUG', True)
+SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI',"sqlite:///"+ BASE_DIR + "/app.db")
+SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS',True)
 UPLOAD_PATH = '/static/uploads/cover-images/'
 UPLOAD_FOLDER = BASE_DIR + UPLOAD_PATH
