4378bb8f4aa816e355ea1c416c2629f71099de01
Brandon Chez <chezmix@gmail.com>
2015-10-29 11:31:51 -0400

    remove timezone from schema

--- migrations/versions/887862cdc2a_.py
+++ migrations/versions/887862cdc2a_.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 56e045c8e129
+Revision ID: 887862cdc2a
 Revises: None
-Create Date: 2015-10-29 01:17:18.388279
+Create Date: 2015-10-29 11:29:06.138242
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '56e045c8e129'
+revision = '887862cdc2a'
 down_revision = None
 
 from alembic import op
@@ -38,9 +38,9 @@ def upgrade():
     sa.Column('long_description', sa.Text(), nullable=False),
     sa.Column('goal_amount', sa.Integer(), nullable=False),
     sa.Column('image_filename', sa.String(length=128), nullable=False),
-    sa.Column('time_created', sa.DateTime(timezone=True), nullable=False),
-    sa.Column('time_start', sa.DateTime(timezone=True), nullable=False),
-    sa.Column('time_end', sa.DateTime(timezone=True), nullable=False),
+    sa.Column('time_created', sa.DateTime(), nullable=False),
+    sa.Column('time_start', sa.DateTime(), nullable=False),
+    sa.Column('time_end', sa.DateTime(), nullable=False),
     sa.ForeignKeyConstraint(['member_id'], ['member.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
@@ -49,7 +49,7 @@ def upgrade():
     sa.Column('member_id', sa.Integer(), nullable=False),
     sa.Column('project_id', sa.Integer(), nullable=False),
     sa.Column('amount', sa.Integer(), nullable=False),
-    sa.Column('time_created', sa.DateTime(timezone=True), nullable=False),
+    sa.Column('time_created', sa.DateTime(), nullable=False),
     sa.ForeignKeyConstraint(['member_id'], ['member.id'], ),
     sa.ForeignKeyConstraint(['project_id'], ['project.id'], ),
     sa.PrimaryKeyConstraint('id')
