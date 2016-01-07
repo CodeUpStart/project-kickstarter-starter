0c536e54e0ab930e8dda7391121276859d73fb13
brandon <brandon@codeupstart.com>
2016-01-07 14:53:04 -0500

    Remove views.py

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, url_for, abort
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
+import datetime
+import cloudinary.uploader
 
 # Initialze main app
 app = Flask(__name__)
@@ -13,7 +15,137 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-import punchstarter.views
+from punchstarter.models import Member, Project, Pledge
+from punchstarter.util import allowed_file
+
+@app.route('/')
+def index():
+	projects = db.session.query(Project).order_by(Project.time_created.desc()).limit(10)
+
+	return render_template('index.html', projects=projects)
+
+@app.route('/projects/create/', methods=['GET', 'POST'])
+def create():
+	if request.method == "POST":
+		
+		# Hardcode guest creator member for now
+		guest_creator = db.session.query(Member).filter_by(id=1).one()
+		now = datetime.datetime.now()
+
+		# Upload cover image
+		cover_image = request.files['cover_image']
+		if cover_image and allowed_file(cover_image.filename):
+			uploaded_image = cloudinary.uploader.upload(
+				cover_image,
+				crop = 'limit',
+				width = 680,
+				height = 550,
+			)
+			image_filename = uploaded_image["public_id"]
+		else:
+			flash("Invalid cover photo file")
+			return redirect(url_for('create'))
+
+
+		if request.form.get("funding_end_date"):
+			time_end = datetime.datetime.strptime(request.form.get("funding_end_date"), "%Y-%m-%d")
+		else:
+			time_end = now + datetime.timedelta(days=7)
+
+		goal_amount = request.form.get("funding_goal") or 10000
+
+		new_project = Project(
+			member_id = guest_creator.id,
+			name = request.form.get("project_name"),
+			short_description = request.form.get("short_description"),
+			long_description = request.form.get("long_description"),
+			goal_amount = goal_amount,
+			image_filename = image_filename,
+			time_created = now,
+			time_start = now,
+			time_end = time_end,
+		)
+		db.session.add(new_project)
+		message = "Your project has been succesfully created!"
+		try:
+			db.session.commit()
+		except Exception, e:
+			db.session.rollback()
+			message = "An error has occurred while creating your project."
+			if app.config["DEBUG"]:
+				message += str(e)
+			flash(message)
+			return redirect(url_for('create'))
+		else:
+			flash(message)
+			return redirect(url_for('project_detail', project_id=new_project.id))			
+
+	elif request.method == "GET":
+		return render_template('create.html')
+
+@app.route('/projects/<int:project_id>/')
+def project_detail(project_id):
+	project = db.session.query(Project).get(project_id)
+	if project is None:
+		abort(404)
+
+	return render_template('project_detail.html', project=project)
+
+@app.route('/projects/<int:project_id>/pledge/', methods=['GET', 'POST'])
+def pledge(project_id):
+	project = db.session.query(Project).get(project_id)
+	if project is None:
+		abort(404)
+
+	if request.method == "POST":
+		
+		# Hardcode guest pledgor member for now
+		guest_creator = db.session.query(Member).filter_by(id=2).one()
+
+		amount = request.form.get("amount")
+		now = datetime.datetime.now()
+
+		new_pledge = Pledge(
+			member_id = guest_creator.id,
+			project_id = project.id,
+			amount = amount,
+			time_created = now,
+		)
+		db.session.add(new_pledge)
+		message = "You have successfully pledged $%s to %s!" % (amount, project.name)
+		try:
+			db.session.commit()
+		except Exception, e:
+			db.session.rollback()
+			message = "An error has occurred while pledging."
+			if app.config["DEBUG"]:
+				message += str(e)
+			flash(message)
+			return redirect(url_for('pledge'), project_id=project.id)
+		else:
+			flash(message)
+			return redirect(url_for('project_detail', project_id=project.id))			
+
+	elif request.method == "GET":
+		return render_template('pledge.html', project=project)
+
+@app.route('/search/')
+def search():
+	query = request.args.get("q") or ""
+	projects = db.session.query(Project).filter(
+		Project.name.ilike('%'+query+'%') |
+		Project.short_description.ilike('%'+query+'%') |
+		Project.long_description.ilike('%'+query+'%')
+	).all()
+	project_count = len(projects)
+
+	query_text = query if query != "" else "all projects"
+
+	return render_template('search.html', 
+		query_text=query_text,
+		projects=projects,
+		project_count=project_count
+	)	
 
 # Set the secret key.  Make sure this is unique for your project!:
 app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
